637887574bd98b2d10358aba70e98f4f1e46bbaa
gohias <EMAIL>
2022-04-17 02:51:55 +0000

    color change

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const TOKENS_PER_MINT = 200; // maximum number of NFTs a user can mint in a sing
 const PUBLIC_MINT_START_DATE = "2022-05-20T18:00:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-16T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x8b19703C663e4eF9540906E09a3B4E3B856d99bb"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
